a732a214b29cb70dcfbc8422f63f22ae14265d51
miriam-moztap <ada_hack16@hackwomen.dev>
2023-12-12 15:03:52 -0600

    se hace el ordenamiento de todos los datos

--- src/componentes/ordenamiento.js
+++ src/componentes/ordenamiento.js
@@ -1,7 +1,7 @@
 import { PruebaHome } from '../views/Home.js';
 import { renderItems } from './cards.js';
 
-export const estructuraOrdemaniento = (data) => {
+export const estructuraOrdemaniento = () => {
     const etiqueta = document.createElement("label");
     etiqueta.textContent = "Ordenar:";
     etiqueta.setAttribute("for", "ordenado");
@@ -9,24 +9,25 @@ export const estructuraOrdemaniento = (data) => {
     orden.setAttribute("data-testid", "select-sort");
     orden.setAttribute("name", "sort-order");
     orden.setAttribute("id", "ordenado");
-  
+
     const opcion1 = document.createElement("option");
     opcion1.setAttribute("value", "blanco2");
     opcion1.textContent = "Selecciona una opción";
-  
+
     const opcion2 = document.createElement("option");
     opcion2.setAttribute("value", "asc");
     opcion2.textContent = "A - Z";
-  
+
     const opcion3 = document.createElement("option");
     opcion3.setAttribute("value", "desc");
     opcion3.textContent = "Z - A";
-  
+
     orden.appendChild(opcion1);
     orden.appendChild(opcion2);
     orden.appendChild(opcion3);
-  
-    etiqueta.appendChild(orden);
 
-return etiqueta;
+    etiqueta.appendChild(orden);
+    
+    return etiqueta;
+    
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -12,20 +12,32 @@ export function PruebaHome(props) {
   const homeContenedor = document.createElement("div");
   homeContenedor.appendChild(header());
   homeContenedor.appendChild(estructuraFiltro());
-  homeContenedor.appendChild(estructuraOrdemaniento(data));
+  homeContenedor.appendChild(estructuraOrdemaniento());
 
+  //Contenedor de estadística
   const datosContados = estadistica(data);
   const contenedorDatosContados = document.createElement("div");
   contenedorDatosContados.appendChild(datosContados);
   homeContenedor.appendChild(contenedorDatosContados);
 
+  //Contenedor filtro
   const tarjetas = renderItems(data);
   const contenedorTarjetas = document.createElement("div");
   contenedorTarjetas.appendChild(tarjetas);
   homeContenedor.appendChild(contenedorTarjetas);
 
+  //pie de página
   homeContenedor.appendChild(pieDePagina());
 
+  //ordenado de todas las tarjetas
+  const contenedorDatosOrdenados = homeContenedor.querySelector("#ordenado");
+  contenedorDatosOrdenados.addEventListener("change", function (event) {
+    const ordenados = sortData(data, "name", event.target.value);
+    contenedorTarjetas.innerHTML = "";
+    contenedorTarjetas.appendChild(renderItems(ordenados));
+  });
+
+  //estructura de filtrado
   const contenedorFiltro = homeContenedor.querySelector("#genero");
   contenedorFiltro.addEventListener("change", function (event) {
     const generos = filterData(data, "genre", event.target.value);
@@ -35,20 +47,7 @@ export function PruebaHome(props) {
     //estas líneas son de la estadística
     contenedorDatosContados.innerHTML = "";
     contenedorDatosContados.appendChild(estadistica(generos));
-
-    //estas líneas son del ordenamiento
-
-    const contenedorDatosOrdenados = homeContenedor.querySelector("#ordenado");
-    contenedorDatosOrdenados.addEventListener("change", function (event) {
-      
-      const datosOrdenados = document.querySelector('div');
-      const ordenados = sortData(generos, "name", event.target.value);
-      datosOrdenados.innerHTML = ordenados;
-
-      contenedorTarjetas.innerHTML = '';
-      contenedorTarjetas.appendChild(datosOrdenados);
-
-    });
+    
   });
 
   return homeContenedor;
