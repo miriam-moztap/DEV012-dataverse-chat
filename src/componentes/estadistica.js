ce39841ad68e2e32b949dc0c6d5b6c99d3f891a6
miriam-moztap <ada_hack16@hackwomen.dev>
2024-01-05 11:38:29 -0600

    se hace la vista del chat individual y se da funcionalidad al botón de enviar

--- src/componentes/estadistica.js
+++ src/componentes/estadistica.js
@@ -1,12 +1,12 @@
 import { computeStats } from '../dataFunctions.js';
 
 export const estadistica = (data) => {
-  const contenedorEstadistica = document.createElement('div');
-  contenedorEstadistica.className = 'estadistica';
-  contenedorEstadistica.setAttribute('id', 'conteo');
+  const contEst = document.createElement('div');
+  contEst.className = 'estadistica';
+  contEst.setAttribute('id', 'conteo');
   const parrafoEstadística = document.createElement('p');
   const sumaData = computeStats(data);
   parrafoEstadística.innerHTML = `Total de Albums: ${sumaData}`;
-  contenedorEstadistica.appendChild(parrafoEstadística);
-  return contenedorEstadistica;
+  contEst.appendChild(parrafoEstadística);
+  return contEst;
 };
--- src/views/Personaje.js
+++ src/views/Personaje.js
@@ -1,38 +1,57 @@
 /* eslint-disable import/prefer-default-export */
 import { header } from '../componentes/header.js';
 import { pieDePagina } from '../componentes/footer.js';
-import { formulario } from '../componentes/formulario.js';
+import { chat } from '../componentes/formulario.js';
+import { formularioEnviar } from '../componentes/formulario.js';
 export function Personaje(props) {
   // Accedemos a las propiedades
   const d = document;
-  const contenedorPersonaje = d.createElement('div');
-  contenedorPersonaje.className = "contenedorPersonaje";
-  contenedorPersonaje.appendChild(header());
+  const contPer = d.createElement('div');
+  contPer.className = "contPer";
+  contPer.appendChild(header());
 
   //CONTENEDOR para almacenar el formulario y el los datos del personaje
   const chatPersonaje = d.createElement('section');
-  chatPersonaje.className='chatPersonaje';
+  chatPersonaje.className = 'chatPersonaje';
 
   const { name, shortDescription, imageUrl } = props;
-  console.log(props);
+
   // Renderizamos nuevamente la información detallada
   const detailElement = d.createElement('div');
-  detailElement.innerHTML = 
-  `<li class="li" itemscope itemtype="Artist">
+  detailElement.innerHTML =
+    `<li class="li" itemscope itemtype="Artist">
     <h2>${name}</h2>
     <img class="imag" src="${imageUrl}" alt="imagen"/>
     <dt><strong>Descripción:</strong></dt><dd itemprop="description">${shortDescription}</dd>
   </li>
       `;
-  
+  const contDetails = d.createElement('div');
+  contDetails.className='contDetails';
+  contDetails.appendChild(detailElement)
+  chatPersonaje.appendChild(contDetails);
+  const contChat = d.createElement('section');
+  contChat.appendChild(chat());
+  contChat.className = 'contChat';
+  contChat.appendChild(formularioEnviar());
+  chatPersonaje.appendChild(contChat);
+  contPer.appendChild(chatPersonaje);
+  contPer.appendChild(pieDePagina());
 
-  chatPersonaje.appendChild(detailElement);
-  const contenedorFormulario = d.createElement('section');
-  contenedorFormulario.appendChild(formulario());
-  contenedorFormulario.className= 'formularioPersonaje';
-  chatPersonaje.appendChild(contenedorFormulario);
-  contenedorPersonaje.appendChild(chatPersonaje);
-  contenedorPersonaje.appendChild(pieDePagina());
 
-  return contenedorPersonaje;
+  //funcionalidad para enviar el mensaje escrito en el text área a la zona de chat
+  const textArea = contPer.querySelector('#textArea');
+  const botonEnviar = contPer.querySelector('#botonEnviar');
+  const cajaChat = contPer.querySelector('#chat');
+  botonEnviar.addEventListener('click', function () {
+    if (textArea.value === '') {
+      alert('Escribe un mensaje antes de enviar')
+      return;
+    }
+    const Mensaje = d.createElement('div');
+    Mensaje.className='mensaje';
+    Mensaje.textContent = textArea.value;
+    cajaChat.insertAdjacentElement('beforeend', Mensaje);
+    textArea.value = '';
+  });
+  return contPer;
 }
