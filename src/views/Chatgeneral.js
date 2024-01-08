b5c833ba2254fdbd574ad5b09b7e64fe14dbec10
miriam-moztap <ada_hack16@hackwomen.dev>
2024-01-08 16:36:15 -0600

    se hace el diseño de los filtros resonsivo

--- src/views/Chatgeneral.js
+++ src/views/Chatgeneral.js
@@ -4,16 +4,33 @@ import { header } from "../componentes/header.js";
 import { pieDePagina } from "../componentes/footer.js";
 
 export function chatgeneral() {
-  const chatView = document.createElement("div");
+  const d = document;
+  const chatView = d.createElement("div");
   chatView.appendChild(header());
 
-  const cajaPrincipal = document.createElement('div');
+  const cajaPrincipal = d.createElement('div');
   cajaPrincipal.className = 'cajaPrincipalChatG';
   cajaPrincipal.appendChild(chat());
   cajaPrincipal.appendChild(formularioEnviar());
   chatView.appendChild(cajaPrincipal);
 
   chatView.appendChild(pieDePagina());
+
+
+  const textArea = chatView.querySelector('#textArea');
+  const botonEnviar = chatView.querySelector('#botonEnviar');
+  const cajaChat = chatView.querySelector('#chat');
+  botonEnviar.addEventListener('click', function () {
+    if (textArea.value === '') {
+      alert('Escribe un mensaje antes de enviar')
+      return;
+    }
+    const Mensaje = d.createElement('div');
+    Mensaje.className='mensaje';
+    Mensaje.textContent = textArea.value;
+    cajaChat.insertAdjacentElement('beforeend', Mensaje);
+    textArea.value = '';
+  });
 
   return chatView;
 }
