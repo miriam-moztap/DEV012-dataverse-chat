7a2b626a092263b4fc1a1e803b1e95d386be726a
Chío <claseschiounam@gmail.com>
2024-01-17 21:49:35 -0600

    errores de indentación

--- src/views/Chatgeneral.js
+++ src/views/Chatgeneral.js
@@ -1,30 +1,30 @@
-import { chat, formularioEnviar } from '../componentes/formulario.js';
-import { botonEnviar } from '../componentes/botones.js';
-import { header } from '../componentes/header.js';
-import { pieDePagina } from '../componentes/footer.js';
-import { iconHome } from '../componentes/iconos.js';
-import { navigateTo } from '../router.js';
-import { getCompletion } from '../lib/API.js';
-import data from '../data/dataset.js';
-import { renderItems } from '../componentes/cards.js';
+import { chat, formularioEnviar } from "../componentes/formulario.js";
+import { botonEnviar } from "../componentes/botones.js";
+import { header } from "../componentes/header.js";
+import { pieDePagina } from "../componentes/footer.js";
+import { iconHome } from "../componentes/iconos.js";
+import { navigateTo } from "../router.js";
+import { getCompletion } from "../lib/API.js";
+import data from "../data/dataset.js";
+import { renderItems } from "../componentes/cards.js";
 
 export function chatgeneral() {
   const d = document;
-  const chatView = d.createElement('div');
-  const iconoChatGeneral = d.createElement('div');
-  iconoChatGeneral.className = 'IconoApi';
+  const chatView = d.createElement("div");
+  const iconoChatGeneral = d.createElement("div");
+  iconoChatGeneral.className = "IconoApi";
   iconoChatGeneral.appendChild(iconHome());
   chatView.appendChild(iconoChatGeneral);
   chatView.appendChild(header());
-  const otraCaja = d.createElement('div');
+  const otraCaja = d.createElement("div");
   otraCaja.className = "otraCaja";
-  const cajaPrincipal = d.createElement('div');
-  cajaPrincipal.className = 'cajaPrincipalChatG';
-  const divFormulario = d.createElement('div');
+  const cajaPrincipal = d.createElement("div");
+  cajaPrincipal.className = "cajaPrincipalChatG";
+  const divFormulario = d.createElement("div");
   divFormulario.appendChild(chat());
   divFormulario.appendChild(formularioEnviar());
-  const botones = d.createElement('div');
-  botones.className = 'botonesP';
+  const botones = d.createElement("div");
+  botones.className = "botonesP";
   botones.appendChild(botonEnviar());
   divFormulario.appendChild(botones);
   cajaPrincipal.appendChild(divFormulario);
@@ -37,52 +37,52 @@ export function chatgeneral() {
   chatView.appendChild(otraCaja);
   chatView.appendChild(pieDePagina());
 
-  const textArea = chatView.querySelector('#textArea');
-  const buttonEnviar = chatView.querySelector('#botonEnviar');
-  const cajaChat = chatView.querySelector('#chat');
-  buttonEnviar.addEventListener('click', () => {
-    if (textArea.value === '') {
-      alert('Escribe un mensaje antes de enviar');
+  const textArea = chatView.querySelector("#textArea");
+  const buttonEnviar = chatView.querySelector("#botonEnviar");
+  const cajaChat = chatView.querySelector("#chat");
+  buttonEnviar.addEventListener("click", () => {
+    if (textArea.value === "") {
+      alert("Escribe un mensaje antes de enviar");
       return;
     }
-    const Mensaje = d.createElement('div');
-    Mensaje.className = 'mensaje';
+    const Mensaje = d.createElement("div");
+    Mensaje.className = "mensaje";
     Mensaje.textContent = textArea.value;
-    cajaChat.insertAdjacentElement('beforeend', Mensaje);
-    textArea.value = '';
+    cajaChat.insertAdjacentElement("beforeend", Mensaje);
+    textArea.value = "";
 
-    const clave = localStorage.getItem('chatGptApiKey');
+    const clave = localStorage.getItem("chatGptApiKey");
     if (!clave) {
-      navigateTo('/apikey');
+      navigateTo("/apikey");
       return;
     }
-    const errorApi = d.createElement('button');
-    errorApi.className = 'errorApi';
-    errorApi.textContent = 'La API Key es incorrecta, da click aquí para volver a introducirla';
-    errorApi.addEventListener('click', () => {
-      navigateTo('/apikey');
+    const errorApi = d.createElement("button");
+    errorApi.className = "errorApi";
+    errorApi.textContent =
+      "La API Key es incorrecta, da click aquí para volver a introducirla";
+    errorApi.addEventListener("click", () => {
+      navigateTo("/apikey");
     });
 
     for (const element of data) {
-
       getCompletion(textArea.value, element.name)
         .then((respuesta) => respuesta.json())
         .then((respuestaArtista) => {
           const response = respuestaArtista.choices[0].message.content;
-          textArea.value = '';
-          const mensajeArtista = d.createElement('div');
-          mensajeArtista.className = 'mensajeArtista';
+          textArea.value = "";
+          const mensajeArtista = d.createElement("div");
+          mensajeArtista.className = "mensajeArtista";
           mensajeArtista.innerHTML = response;
           cajaChat.appendChild(mensajeArtista);
         })
         .catch((error) => {
           if (error) {
-            
+            botones.appendChild(errorApi);
           }
-          botones.appendChild(errorApi);
+          
         });
     }
-  });//click
+  }); //click
 
   return chatView;
 }
